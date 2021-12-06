c2b28735c708496cbc691c56b32332a293f645e4
shanequaid-toast <shane.quaid@toasttab.com>
2021-12-06 22:03:41 +0000

    Test branch

--- src/main/java/com/baeldung/resource/ResourceServerApp.java
+++ src/main/java/com/baeldung/resource/ResourceServerApp.java
@@ -1,12 +1,24 @@
 package com.baeldung.resource;
 
+import org.keycloak.adapters.AdapterDeploymentContext;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springsecurity.AdapterDeploymentContextFactoryBean;
+import org.keycloak.adapters.springsecurity.config.KeycloakSpringConfigResolverWrapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.InputStreamSource;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @SpringBootApplication
-public class ResourceServerApp {
+public class ResourceServerApp implements WebMvcConfigurer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ResourceServerApp.class, args);
@@ -15,6 +27,20 @@ public class ResourceServerApp {
     @Bean
     public ModelMapper getMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        registry.viewResolver(resolver);
     }
     
 }
