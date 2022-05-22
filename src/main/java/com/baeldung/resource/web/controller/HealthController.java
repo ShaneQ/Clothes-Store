0f2699154342414240bf615571ecb083e9370de3
shanequaid-toast <shane.quaid@toasttab.com>
2022-05-22 18:41:11 +0100

    Added health endpoint for load balancer

---
+++ src/main/java/com/baeldung/resource/web/controller/HealthController.java
@@ -0,0 +1,20 @@
+package com.baeldung.resource.web.controller;
+
+import javax.ws.rs.core.Response;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/public/health")
+@AllArgsConstructor
+public class HealthController {
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    public void isUp() {
+    }
+}
